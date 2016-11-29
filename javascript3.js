473b6eb1c908b7ec8e93e4498f13b13d5db6dcc6
deepakrana91 <deepakrana91@live.com>
2016-11-29 12:16:58 +0530

    Add files via upload

---
+++ javascript3.js
@@ -0,0 +1,15 @@
+ 
+ var emp = { name:"deepak rana",age:25,salary:35000,
+address:{city:"roorkee",state:"uttrakhand",pin:247667}};
+
+  object();
+ function object ()
+        {
+            
+            console.log(emp.name);
+            console.log(emp.age);
+            console.log(emp.salary);
+            console.log(emp.address.city);
+            console.log(emp.address.state);
+            console.log(emp.address.pin);
+        }
